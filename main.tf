b5b71304e4cb8a0eadd2d8241569d4f33f37f7d2
Oleksandr Marfut <omarf@softserveinc.com>
2021-11-01 13:15:33 +0100

    service bus topics

--- main.tf
+++ main.tf
@@ -11,10 +11,46 @@ provider "azurerm" {
   features {}
 }
 
-resource "azurerm_resource_group" "rg" {
-  name     = "cqrs"
+//Resource group - cqrs-resourcegroup:
+resource "azurerm_resource_group" "cqrs-resourcegroup" {
+  name     = "cqrs-resourcegroup"
   location = "germanywestcentral"
   tags = {
-    source = "MyTerraform"
+    source = "terraform"
   }
+}
+
+//Service bus namespace - cqrs-servicebus-namespace:
+resource "azurerm_servicebus_namespace" "cqrs-servicebus-namespace" {
+  name                = "cqrs-servicebus-namespace"
+  location            = "germanywestcentral"
+  resource_group_name = "cqrs-resourcegroup"
+  sku                 = "Standard"
+
+  tags = {
+    source = "terraform"
+  }
+}
+
+//static-data-update-started topic and subscriptions:
+resource "azurerm_servicebus_topic" "static-data-update-started" {
+  name                = "static-data-update-started"
+  resource_group_name = "cqrs-resourcegroup"
+  namespace_name      = "cqrs-servicebus-namespace"
+}
+
+resource "azurerm_servicebus_subscription" "static-data-notification" {
+  name                = "static-data-notification"
+  resource_group_name = "cqrs-resourcegroup"
+  namespace_name      = "cqrs-servicebus-namespace"
+  topic_name          = "static-data-update-started"
+  max_delivery_count  = 1
+}
+
+resource "azurerm_servicebus_subscription" "taxrules-static-data-notification" {
+  name                = "taxrules-static-data-notification"
+  resource_group_name = "cqrs-resourcegroup"
+  namespace_name      = "cqrs-servicebus-namespace"
+  topic_name          = "static-data-update-started"
+  max_delivery_count  = 1
 }
